794602c8a4d983f2fbc360720d7574cd032b81ab
nicolaparutto <nicolaparutto2003@gmail.com>
2025-01-27 16:38:34 +0100

    feat: api call made, data received & PostList structure added

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,9 +1,35 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
+
 function PostsList() {
+  const defaultApiUrl = "http://localhost:3000"
+
+  const [postsData, setPostsData] = useState(null);
+
+  const fetchPosts = () => {
+    axios.get(`${defaultApiUrl}/posts`)
+      .then(res => {
+        setPostsData(res.data)
+      })
+  }
+
+  useEffect(() => {
+    fetchPosts()
+  }, [])
 
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
+    <section className="container">
+      <div className="cards-container">
+        <div className="card">
+          <div className="card-image">
+
+          </div>
+          <div className="card-text">
+
+          </div>
+        </div>
+      </div>
+    </section>
   )
 
 }
